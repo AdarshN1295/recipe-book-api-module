1d2658aa1c8cd385211792c0d125b99d593d8da5
Adarsh Nigam <150742618+AdarshN1295@users.noreply.github.com>
2025-02-17 00:53:17 +0530

    Update recipeData.js

--- recipeData.js
+++ recipeData.js
@@ -1,1 +1,7 @@
-
+const recipe = [
+  {
+    "id" : 1,
+    "name" : "candle",
+    "procedure" : "melt the wax, add the fragrance with essential oils, place the wick and pour the wax in mold and wait for it , to make it strong."
+  }
+]
